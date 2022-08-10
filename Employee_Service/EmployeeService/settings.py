1b1ae89baea08f4ca94797778adf48b11d26921a
Ashwin Patil <ashwin.mpatil98@gmail.com>
2022-08-10 21:46:09 +0530

    Secret confidential

--- Employee_Service/EmployeeService/settings.py
+++ Employee_Service/EmployeeService/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h$__(lr+j$ojn3!1^$l=a@8g&jv%bhin2h3p-s-(*leg*x0o=h'
+SECRET_KEY = '****'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
